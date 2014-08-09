a3c9543aef07ce1a5b6b6aba0634ecd9629cc3b9
Fengyuan Chen <cfy.net@gmail.com>
2014-08-09 16:28:25 +0800

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,20 @@ module.exports = function(grunt) {
 
     "use strict";
 
-    var pkg = grunt.file.readJSON("package.json"),
-        key;
-
     grunt.initConfig({
-        pkg: pkg,
+        pkg: grunt.file.readJSON("package.json"),
         banner: "/*!\n" +
-                " * <%= pkg.title %> v<%= pkg.version %>\n" +
+                " * Datepicker v<%= pkg.version %>\n" +
                 " * <%= pkg.homepage %>\n" +
                 " *\n" +
-                " * Copyright <%= grunt.template.today('yyyy') %> <%= pkg.author %>\n" +
-                " * Released under the <%= pkg.license %> license\n" +
+                " * Copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
+                " * Released under the <%= pkg.license.type %> license\n" +
                 " */\n",
         clean: {
-            files: ["build/<%= pkg.version %>", "dist/"]
+            dist: ["dist/"],
+            build: ["build/<%= pkg.version %>.<%= grunt.template.today('yyyymmdd') %>"],
+            release: ["release/<%= pkg.version %>"],
+            docs: ["../fengyuanchen.github.io/<%= pkg.name %>/"]
         },
         jshint: {
             options: {
@@ -27,6 +27,27 @@ module.exports = function(grunt) {
             dist: {
                 src: "src/<%= pkg.name %>.js",
                 dest: "dist/<%= pkg.name %>.min.js"
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ["last 2 versions", "ie 8", "ie 9", "android 2.3", "android 4", "opera 12"]
+            },
+            core: {
+                options: {
+                    map: false
+                },
+                src: "dist/<%= pkg.name %>.css",
+                dest: "dist/<%= pkg.name %>.css"
+            }
+        },
+        csscomb: {
+            options: {
+                config: ".csscomb.json"
+            },
+            core: {
+                src: "dist/<%= pkg.name %>.css",
+                dest: "dist/<%= pkg.name %>.css"
             }
         },
         csslint: {
@@ -60,7 +81,28 @@ module.exports = function(grunt) {
                 expand: true,
                 cwd: "dist/",
                 src: "**",
-                dest: "build/<%= pkg.version %>/",
+                dest: "build/<%= pkg.version %>.<%= grunt.template.today('yyyymmdd') %>/",
+                filter: "isFile"
+            },
+            release: {
+                expand: true,
+                cwd: "dist/",
+                src: "**",
+                dest: "release/<%= pkg.version %>/",
+                filter: "isFile"
+            },
+            sync: {
+                expand: true,
+                cwd: "dist/",
+                src: "**",
+                dest: "../fengyuanchen.github.io/dist/",
+                filter: "isFile"
+            },
+            docs: {
+                expand: true,
+                cwd: "docs/",
+                src: "**",
+                dest: "../fengyuanchen.github.io/<%= pkg.name %>/",
                 filter: "isFile"
             }
         },
@@ -74,11 +116,9 @@ module.exports = function(grunt) {
     });
 
     // Loading dependencies
-    for (key in pkg.devDependencies) {
-        if (key !== "grunt" && key.indexOf("grunt") === 0) {
-            grunt.loadNpmTasks(key);
-        }
-    }
+    require("load-grunt-tasks")(grunt);
 
-    grunt.registerTask("default", ["clean", "jshint", "uglify", "csslint", "cssmin", "copy:dist", "usebanner", "copy:build"]);
+    grunt.registerTask("release", ["clean:release", "copy:release"]);
+    grunt.registerTask("docs", ["clean:docs", "copy:sync", "copy:docs"]);
+    grunt.registerTask("default", ["clean:dist", "clean:build", "jshint", "uglify", "copy:dist", "autoprefixer", "csscomb", "csslint", "cssmin", "usebanner", "copy:build"]);
 };
