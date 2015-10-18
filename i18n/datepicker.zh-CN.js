daa926e2ecb779f2e4ea216c4117e5395d9c3f25
Fengyuan Chen <cfy.org@gmail.com>
2015-10-18 14:03:27 +0800

    Updated i18n files

--- i18n/datepicker.zh-CN.js
+++ i18n/datepicker.zh-CN.js
@@ -1,32 +1,27 @@
-/*!
- * Datepicker v0.1.0
- * https://github.com/fengyuanchen/datepicker
- *
- * Copyright 2014 Fenngyuan Chen
- * Released under the MIT license
- */
-
 (function (factory) {
-    if (typeof define === "function" && define.amd) {
-        define(["jquery"], factory);
-    } else {
-        factory(jQuery);
-    }
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as anonymous module.
+    define('datepicker.zh-CN', ['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node / CommonJS
+    factory(require('jquery'));
+  } else {
+    // Browser globals.
+    factory(jQuery);
+  }
 })(function ($) {
 
-    "use strict";
+  'use strict';
 
-    $.fn.datepicker.setDefaults({
-        autoClose: false,
-        dateFormat: "yyyy-mm-dd",
-        days: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
-        daysShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
-        daysMin: ["日", "一", "二", "三", "四", "五", "六"],
-        months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-        monthsShort: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
-        showMonthAfterYear: true,
-        viewStart: 0, // days
-        weekStart: 1, // Monday
-        yearSuffix: "年"
-    });
+  $.fn.datepicker.languages['zh-CN'] = {
+    format: 'yyyy年mm月dd日',
+    days: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+    daysShort: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+    daysMin: ['日', '一', '二', '三', '四', '五', '六'],
+    months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+    monthsShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    weekStart: 1,
+    yearFirst: true,
+    yearSuffix: '年'
+  };
 });
