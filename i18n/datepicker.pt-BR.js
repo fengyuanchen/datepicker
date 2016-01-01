4f8cf37cc688d8b4e353bc65c9a09167f904877e
Fengyuan Chen <cfy.org@gmail.com>
2016-01-01 11:22:13 +0800

    Remove default options

--- i18n/datepicker.pt-BR.js
+++ i18n/datepicker.pt-BR.js
@@ -19,9 +19,6 @@
     daysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
     daysMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
     months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-    weekStart: 0,
-    yearFirst: false,
-    yearSuffix: ''
+    monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
   };
 });
