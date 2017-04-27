0b9dd3af854d55f6bf6edf9d3e5cedc00c839a79
Víctor Díaz <victor@victordiaz.me>
2017-04-27 20:37:48 +0200

    In Spanish the first day of the week is Monday.
    Source: https://es.wikipedia.org/wiki/Semana

--- i18n/datepicker.es-ES.js
+++ i18n/datepicker.es-ES.js
@@ -18,7 +18,7 @@
     days: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
     daysShort: ['Dom','Lun','Mar','Mie','Jue','Vie','Sab'],
     daysMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sa'],
-    weekStart: 0,
+    weekStart: 1,
     months: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
     monthsShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic']
   };
