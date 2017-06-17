da6e13903b6e14e2c1aee1399195ef0d7a996e65
Chen Fengyuan <chenfengyuan@outlook.com>
2017-06-17 12:35:00 +0800

    Remove old files

--- test/options/autopick.js
+++
@@ -1,9 +0,0 @@
-QUnit.test('options.autoPick', function (assert) {
-  var $input = window.createInput();
-
-  $input.datepicker({
-    autoPick: true
-  });
-
-  assert.equal($input.val(), $input.datepicker('getDate', true));
-});
--- test/options/autoshow.js
+++
@@ -1,9 +0,0 @@
-QUnit.test('options.autoShow', function (assert) {
-  var $input = window.createInput();
-  var datepicker = $input.datepicker({
-        autoShow: true
-      }).data('datepicker');
-
-  assert.ok(datepicker.$picker.is(':visible'));
-  $input.datepicker('hide');
-});
