8999f1e40450c9fafa8140e490105198df986e7e
Wooseop Kim <wooseop.kim.dev@gmail.com>
2017-07-04 01:37:16 +0900

    Add Korean (ko-KR) language

---
+++ i18n/datepicker.ko-KR.js
@@ -0,0 +1,27 @@
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as anonymous module.
+    define('datepicker.ko-KR', ['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node / CommonJS
+    factory(require('jquery'));
+  } else {
+    // Browser globals.
+    factory(jQuery);
+  }
+})(function ($) {
+
+  'use strict';
+
+  $.fn.datepicker.languages['ko-KR'] = {
+    format: 'yyyy. mm. dd',
+    days: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+    daysShort: ['일', '월', '화', '수', '목', '금', '토'],
+    daysMin: ['일', '월', '화', '수', '목', '금', '토'],
+    months: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    monthsShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    weekStart: 1,
+    yearFirst: true,
+    yearSuffix: '년'
+  };
+});
