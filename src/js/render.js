11c93071dcd369b36cf00881a64a01f8184a5fcb
Chen Fengyuan <chenfengyuan@outlook.com>
2018-08-05 10:51:25 +0800

    feat: show full month name in day picker
    Fixed #133, Closes #154

--- src/js/render.js
+++ src/js/render.js
@@ -157,7 +157,7 @@ export default {
     const {
       disabledClass,
       filter,
-      monthsShort,
+      months,
       weekStart,
       yearSuffix,
     } = options;
@@ -334,8 +334,8 @@ export default {
     this.$monthCurrent
       .toggleClass(disabledClass, prevDisabled && nextDisabled)
       .html(options.yearFirst
-        ? `${viewYear + yearSuffix} ${monthsShort[viewMonth]}`
-        : `${monthsShort[viewMonth]} ${viewYear}${yearSuffix}`);
+        ? `${viewYear + yearSuffix} ${months[viewMonth]}`
+        : `${months[viewMonth]} ${viewYear}${yearSuffix}`);
     this.$days.html(prevItems.join('') + items.join('') + nextItems.join(''));
   },
 };
