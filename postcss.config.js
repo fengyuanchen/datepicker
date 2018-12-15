b1f5d60ba04b3ad51758d94f0a1a4e499438c663
Chen Fengyuan <chenfengyuan@outlook.com>
2018-12-15 11:51:41 +0800

    build: add stylelint

--- postcss.config.js
+++ postcss.config.js
@@ -12,7 +12,8 @@ module.exports = {
     'postcss-header': {
       header: rollupConfig.output[0].banner,
     },
-    autoprefixer: {},
-    stylefmt: {},
+    stylelint: {
+      fix: true,
+    },
   },
 };
